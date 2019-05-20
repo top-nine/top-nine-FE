ef05c662892e0d2e249d179e5fb4a3df84ac42e8
Christopher Riffle <cmr629@gmail.com>
2019-05-20 14:58:53 -0400

    Login action and reducer code

--- top-nine/src/actions/index.js
+++ top-nine/src/actions/index.js
@@ -1,1 +1,18 @@
 import axios from 'axios';
+
+export const LOGIN = "LOGIN";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILED = "LOGIN_FAILED";
+
+export const loginuser = (cred, callback) => dispatch => {
+    const { username, password } = cred;
+    dispatch({ type: LOGIN });
+    const request = axios.post('http://localhost:5000/api/login', { username, password });
+    request.then(function (response) {
+        dispatch({ type: LOGIN_SUCCESS, payload: response.data.payload });
+    })
+        .catch(function (error) {
+            dispatch({ type: LOGIN_FAILED, payload: error });
+        });
+};
+
--- top-nine/src/reducers/index.js
+++ top-nine/src/reducers/index.js
@@ -1,6 +1,29 @@
-const initialState = {};
+import { LOGIN, LOGIN_SUCCESS, LOGIN_FAILED } from "../actions";
+
+const initialState = {
+  user: [],
+  topNine: [],
+  loggingIn: false,
+  error: null
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case LOGIN_SUCCESS:
+      window.localStorage.setItem("auth", action.payload);
+      return {
+        ...state,
+        loggingIn: true,
+        user: action.payload
+      };
+    case LOGIN_FAILED:
+      return {
+        ...state,
+        error: action.payload,
+        loggingIn: false
+      };
+
+    default:
+      return state;
   }
 };
